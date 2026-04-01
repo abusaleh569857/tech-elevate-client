15e2c642e6e9438150287c6d8857707418f41531
Abusaleh Alam Khan <abusaleh.cse.uiu@gmail.com>
2026-04-01 23:48:00 +0600

    feat: initialize application entry point and define public routing configuration

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from "react";
+
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "@/app/App.jsx";
@@ -7,13 +7,12 @@ import AuthProvider from "@/app/providers/AuthProvider.jsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
+  
     <Provider store={store}>
       <AuthProvider>
         <App />
       </AuthProvider>
     </Provider>
-  </StrictMode>
 );
 
 
--- src/routes/publicRoutes.jsx
+++ src/routes/publicRoutes.jsx
@@ -25,4 +25,3 @@ export const publicRoutes = [
     element: withLazy(Login),
   },
 ];
-
